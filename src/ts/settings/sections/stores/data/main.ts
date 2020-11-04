ec06a0b72a20243ed9dfdbeb0ade5bd3cfb565ed
loftyshaky <loftyshaky@gmail.com>
2020-11-05 06:25:00 +1000

    feat(settings): add links section

--- src/ts/settings/sections/stores/data/main.ts
+++ src/ts/settings/sections/stores/data/main.ts
@@ -17,22 +17,35 @@ export class Main {
                 arr: this.i0.sections.settings.inputs,
                 section: 'settings',
             });
+            this.i0.sections.links.inputs = Utils.i.to_object({
+                arr: this.i0.sections.links.inputs,
+                section: 'links',
+            });
         }
 
         return this.i0;
     }
 
-    public sections: any = [new o_inputs.Section({
-        name: 'settings',
-        inputs: [
-            new o_inputs.Textarea({
+    public sections: any = [
+        new o_inputs.Section({
+            name: 'settings',
+            inputs: [
+                new o_inputs.Textarea({
+                    name: 'click_action',
+                    event_callback: () => null,
+                }),
+                new o_inputs.Textarea({
+                    name: 'reload_actions',
+                    event_callback: () => null,
+                }),
+            ],
+        }),
+        new o_inputs.Section({
+            name: 'links',
+            inputs: [new o_inputs.Textarea({
                 name: 'click_action',
                 event_callback: () => null,
-            }),
-            new o_inputs.Textarea({
-                name: 'reload_actions',
-                event_callback: () => null,
-            }),
-        ],
-    })]
+            })],
+        }),
+    ]
 }
