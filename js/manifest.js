d30cc2836bbf695f59ade5ebeb0e4ef2c0dfa829
loftyshaky <loftyshaky@gmail.com>
2020-12-12 10:47:14 +1000

    feat: add key to manifest

--- js/manifest.js
+++ js/manifest.js
@@ -13,6 +13,7 @@ class Manifest {
             name: 'Advanced Extension Reloader',
             description: '__MSG_description__',
             offline_enabled: true,
+            key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA5DLBHROvhHfyyZJYqgVvijGHfLuZdXRHIL0Qm9Tk8PTTpdY6mF/c3Pih+TrCpPZXOI0WePhspKSI6tY7nCPFKe1+2V3iFgKFgxf5wbWHq314oKmXfplLBPIDHcw5gnpsWWDjzziNIdhv4xVXney1oSw7l7N8dCNj5T3b70cbYaVWuDaq4exeid7NrTA1ur0DYm767BXkikvnjla/NsaXSp6Ja9OXPOrFR679nO81m9ul+0nbd9Pw+lEWhF5DLtE4caTr+n2ae9w4WCPlepV1hkw5K5azwxfbdR3GLlgF8w3UdCIWDdsSS5flwF2EzbeVXrWFhig9LIkApW+xBvN1PQIDAQAB',
             background: {
                 page: 'background.html',
                 persistent: true,
