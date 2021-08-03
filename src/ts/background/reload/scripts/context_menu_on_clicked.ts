e6bb4eb6171ae7d2498ae797e72ec5e5d44e821d
loftyshaky <loftyshaky@gmail.com>
2021-08-04 02:32:42 +1000

    feat: wrap function in err

--- src/ts/background/reload/scripts/context_menu_on_clicked.ts
+++ src/ts/background/reload/scripts/context_menu_on_clicked.ts
@@ -2,8 +2,11 @@ import { browser } from 'webextension-polyfill-ts';
 
 import { s_reload } from 'background/internal';
 
-browser.contextMenus.onClicked.addListener(async (info) => {
-    const settings = await ext.storage_get(['click_action', 'reload_actions']);
+browser.contextMenus.onClicked.addListener(
+    (info): Promise<void> =>
+        err_async(async () => {
+            const settings = await ext.storage_get(['click_action', 'reload_actions']);
 
-    s_reload.Watch.i().reload(settings.reload_actions[info.menuItemId]);
-});
+            s_reload.Watch.i().reload(settings.reload_actions[info.menuItemId]);
+        }, 'aer_1055'),
+);
