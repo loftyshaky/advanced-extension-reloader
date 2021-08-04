279f45e717e53c0ca7b43fedd22b2aecee51f205
loftyshaky <loftyshaky@gmail.com>
2021-08-04 17:34:41 +1000

    feat: style background tab

--- src/ts/background_tab/components/body.tsx
+++ src/ts/background_tab/components/body.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 
-export const Body: React.FunctionComponent = observer(() => <div>Background tab</div>);
+export const Body: React.FunctionComponent = observer(() => (
+    <div className='main'>
+        <div className='background_tab_manifest_3_msg'>
+            {ext.msg('background_tab_manifest_3_msg_text')}
+        </div>
+    </div>
+));
