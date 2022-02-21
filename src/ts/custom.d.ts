60242610e26bc5308961c8455c4c816501019b1f
loftyshaky <loftyshaky@gmail.com>
2022-02-21 20:01:36 +1000

    build: add socket.io-client types

--- src/ts/custom.d.ts
+++ src/ts/custom.d.ts
@@ -2,5 +2,3 @@ interface Window {
     page: 'background' | 'settings';
     show_dependencicies_from_other_page_loaded_into_this_page_alert: import('@loftyshaky/shared').t.CallbackVariadicVoid;
 }
-
-declare module 'socket.io-client';
