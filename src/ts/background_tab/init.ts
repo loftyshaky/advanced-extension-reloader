404ae261e886fb21fcf7a76cd88fe02d35f2c1e9
loftyshaky <loftyshaky@gmail.com>
2022-02-21 20:05:14 +1000

    fix: try to connect only once

--- src/ts/background_tab/init.ts
+++ src/ts/background_tab/init.ts
@@ -2,7 +2,6 @@ import { s_reload } from 'background_tab/internal';
 
 export const init = (): void =>
     err(() => {
-        s_reload.Watch.i().connect();
         s_reload.Watch.i().play_sound({ mute: true });
         ext.send_msg({ msg: 'react_to_change' });
     }, 'aer_1000');
