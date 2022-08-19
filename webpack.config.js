c2c57d90afd07762350ed05a791dc970bd99a63f
loftyshaky <loftyshaky@gmail.com>
2022-08-19 12:47:21 +1000

    build: pass env to some functions

--- webpack.config.js
+++ webpack.config.js
@@ -49,13 +49,15 @@ module.exports = (env, argv) => {
             });
         },
         callback_done: () => {
+            const env_2 = 'ext';
+
             manifest.generate({
                 mode: argv.mode,
                 test: env.test,
                 browser: env.browser,
             });
-            env_instance.generate({ browser: env.browser });
-            locales.merge();
+            env_instance.generate({ browser: env.browser, mode: argv.mode, env: env_2 });
+            locales.merge({ env: env_2 });
         },
     });
 
