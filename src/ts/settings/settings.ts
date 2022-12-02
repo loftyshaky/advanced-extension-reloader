e2310f09cd36db13095d138b5d2d3b0f750a56c8
loftyshaky <loftyshaky@gmail.com>
2022-12-03 01:35:07 +1000

    feat: show unable to access settings error

--- src/ts/settings/settings.ts
+++ src/ts/settings/settings.ts
@@ -4,6 +4,7 @@ import { init } from 'settings/internal';
 
 (async () => {
     await d_settings.Transform.i().set_transformed_from_storage();
+    await show_unable_to_access_settings_error({});
 
     init_shared();
     await init();
