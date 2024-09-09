d5b63fbc2b6f72220d9ede3456a9d2104229b2c3
loftyshaky <loftyshaky@gmail.com>
2024-09-09 23:02:57 +1000

    feat(settings): stop forcing input min width

--- src/ts/settings/init.ts
+++ src/ts/settings/init.ts
@@ -1,11 +1,8 @@
 import { InitAll } from 'shared/internal';
-import { d_inputs } from '@loftyshaky/shared/inputs';
 import { d_sections } from 'settings/internal';
 
 export const init = (): Promise<void> =>
     err_async(async () => {
-        d_inputs.InputWidth.set_min_and_max_width({ min_width: 310 });
-
         await InitAll.init();
 
         d_sections.Sections.init();
